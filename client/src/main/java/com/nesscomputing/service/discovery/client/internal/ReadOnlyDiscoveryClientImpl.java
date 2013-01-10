dfb10271b9983ce50b3005e61bb4c9396213c212
Jeremy Schiff <=>
2013-01-10 11:27:11 -0800

    Made the change the world timeout configurable, and the default 10 seconds, not 1, so it works better when talking from the office to the server
    Conflicts:
    	client/src/main/java/com/nesscomputing/service/discovery/client/internal/ReadOnlyDiscoveryClientImpl.java
    	pom.xml

--- client/src/main/java/com/nesscomputing/service/discovery/client/internal/ReadOnlyDiscoveryClientImpl.java
+++ client/src/main/java/com/nesscomputing/service/discovery/client/internal/ReadOnlyDiscoveryClientImpl.java
@@ -18,15 +18,15 @@ package com.nesscomputing.service.discovery.client.internal;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-
-import org.apache.commons.lang3.StringUtils;
-import org.codehaus.jackson.map.ObjectMapper;
-
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Sets;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.google.inject.name.Named;
+
+import org.apache.commons.lang3.StringUtils;
+import org.codehaus.jackson.map.ObjectMapper;
+
 import com.nesscomputing.lifecycle.Lifecycle;
 import com.nesscomputing.lifecycle.LifecycleListener;
 import com.nesscomputing.lifecycle.LifecycleStage;
@@ -106,7 +106,7 @@ public class ReadOnlyDiscoveryClientImpl extends AbstractDiscoveryClient impleme
             LOG.debug("Waiting for the world to change!");
             try {
                 // The magic one second wait for service discovery to catch up.
-                if (waitForWorldChange(1, TimeUnit.SECONDS)) {
+                if (waitForWorldChange(discoveryConfig.getWorldChangeTimeout(), TimeUnit.SECONDS)) {
                     LOG.debug("World just changed!");
                 }
                 else {
